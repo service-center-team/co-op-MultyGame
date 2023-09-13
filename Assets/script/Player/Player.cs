d1619ba2d785574cde8651d49b5b292df0c2480f
Choi Seongmin <chlrjdals@gmail.com>
2023-09-13 16:52:12 +0900

    player move add

---
+++ Assets/script/Player/Player.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("기본 속도")]
+    [SerializeField] private float speed = 10.0f; // 기본 속도 변수
+    [Header("달리기 속도")]
+    [SerializeField] private float runSpeed = 1.5f; // 달리기 속도 변수
+    [Header("점프 파워")]
+    [SerializeField] private float jumpPower = 5.0f; // 점프 파워 변수
+
+    private float hAxis;    // 앞뒤 
+    private float vAxis;    // 좌우
+
+    bool isRun;    // shift 눌렸는지 아닌지 확인
+    bool isJump;    // spaceBar 눌렸는지 아닌지 확인
+    
+    Vector3 moveVec;
+    Rigidbody rigid;
+    
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()//Input을 정리하는 코드
+    {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+    }
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        isRun = Input.GetButton("Run");
+        isJump = Input.GetButtonDown("Jump");
+    }
+
+    void Move()//플레이어 움직임
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        if (isRun)
+            transform.position += moveVec * (speed * runSpeed * Time.deltaTime);//달리기 속도
+        else
+            transform.position += moveVec * (speed * Time.deltaTime);//걷기(외부에서 설정)
+    }
+
+    void Turn()//플레이어가 움직임에 따라 몸 돌기
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()//점프
+    {
+        if (isJump)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            isJump= true;
+        }
+    }
+}
